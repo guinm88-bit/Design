1d0961ec7accfeb80cc20bc50e5606755f860f1e
guinm88-bit <guinm88@gmail.com>
2025-12-22 22:15:46 +0530

    Update home.js

--- home.js
+++ home.js
@@ -1,30 +1,32 @@
 function loadDesigns() {
-  const list = document.getElementById("designList");
-  const designs = JSON.parse(localStorage.getItem("designs") || "[]");
+  const list = JSON.parse(localStorage.getItem("designs") || "[]");
+  const box = document.getElementById("designs");
+  box.innerHTML = "";
 
-  if (designs.length === 0) {
-    list.innerHTML = "<p>No saved designs</p>";
+  if (list.length === 0) {
+    box.innerHTML = "<p>No saved designs</p>";
     return;
   }
 
-  designs.forEach((d, i) => {
+  list.forEach((d, i) => {
     const div = document.createElement("div");
+    div.className = "card";
     div.innerHTML = `
-      <b>${d.name}</b>
+      <b>${d.name}</b><br>
       <button onclick="openDesign(${i})">Open</button>
     `;
-    list.appendChild(div);
+    box.appendChild(div);
   });
 }
 
 function newDesign() {
   localStorage.removeItem("currentDesign");
-  window.location.href = "editor.html";
+  location.href = "editor.html";
 }
 
 function openDesign(i) {
   localStorage.setItem("currentDesign", i);
-  window.location.href = "editor.html";
+  location.href = "editor.html";
 }
 
-window.onload = loadDesigns;
+loadDesigns();
